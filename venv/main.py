0e4b729d716bef4a1302d6e0f636ada7957a42fc
Anonymous-Ivan <jonkovun@gmail.com>
2021-12-27 18:22:20 +0300

    Добавление картинки игрока

--- venv/main.py
+++ venv/main.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, os, sys
 
 pygame.init()
 size = width, height = 500, 800
@@ -6,11 +6,28 @@ FPS = 60
 all_sprites = pygame.sprite.Group()
 screen = pygame.display.set_mode(size)
 
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((50, 60))
-        self.image.fill((255, 0, 0))
+        self.image = player_image
         self.rect = self.image.get_rect()
         self.rect.centerx = width // 2
         self.rect.bottom = height - 20
@@ -29,6 +46,8 @@ class Player(pygame.sprite.Sprite):
         if self.rect.x <= 0:
             self.rect.x = 5
 
+
+player_image = load_image('mario.png', -1)
 player = Player()
 all_sprites.add(player)
 clock = pygame.time.Clock()
